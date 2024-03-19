1d8c51dcf03195337207c0ea6dc692af51871036
Judith Sanchez <judith.sanchez-marty.external@capgemini.com>
2024-03-19 08:35:04 +0000

    last commit

--- webapp/test/unit/AllTests.js
+++ webapp/test/unit/AllTests.js
@@ -1,5 +1,8 @@
 sap.ui.define([
-	"student00comsaptrainingux402qunit/ux402_qunit/test/unit/controller/main.controller"
+
+	"student00comsaptrainingux402qunit/ux402_qunit/test/unit/controller/main.controller",
+	"student00comsaptrainingux402qunit/ux402_qunit/test/unit/model/Formatter"
+	
 ], function () {
 	"use strict";
 });
